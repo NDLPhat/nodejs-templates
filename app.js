269a2d11e8b2884c0eac2c159d2471e2be199eda
phat <nguyenducluongphat7049@gmail.com>
2019-08-20 00:31:43 +0700

    update

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var tourRouter = require("./routes/tourRoute");
 var userRouter = require("./routes/userRoute");
 const app = express();
 var dotenv = require("dotenv");
+var appError = require("./utils/appError");
+var globalError = require("./controller/errorController");
 dotenv.config({path: "./config.env"});
 
 
@@ -19,6 +21,19 @@ app.use((req, res, next) => {
     req.requestTime = new Date().toISOString();
     next();
 })
+app.all("*", (req, res, next) => {
+    // res.status(404).json({
+    //     status: "fail",
+    //     message: `Can't find ${req.originalUrl}`
+    // })
+    // const err = new Error();
+    // err.statusCode = 404;
+    // err.status = "fail";
+    next(new appError(`Can't find ${req.originalUrl}`, 404));
+    // next();
+})
+
+app.use(globalError)
 
 // ROUTES
 app.use("/api/v1/tours", tourRouter);
--- models/tourModels.js
+++ models/tourModels.js
@@ -1,11 +1,14 @@
 var mongoose = require("mongoose");
+var slugify = require("slugify");
+var validator = require("validator");
 
 const tourSchema = new mongoose.Schema({
     name: {
         type: String, 
         required: [true, "A tour must have a name"],
         unique: true,
-        trim: true
+        trim: true,
+        validate: [validator.isAlpha, 'Tour name must only contain character']
     },
     duration: {
         type: Number,
@@ -48,10 +51,53 @@ const tourSchema = new mongoose.Schema({
     images: [String],
     createdAt: {
         type: Date,
-        default: Date.now()
+        default: Date.now(),
+        select: false
     },
-    startDates: [Date]
-});
+    startDates: [Date],
+    slug: String,
+    secretTour: {
+        type: Boolean,
+        default: false
+    },
+    priceDiscount: {
+        type: Number,
+        validate: {
+            validator: function(value){
+                return value < this.price
+            },
+            message: `Discout price (${this.price}) should be below regular price`
+        }
+        
+    }
+},{
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true }
+}
+);
+
+tourSchema.virtual("durationWeeks").get(function(){
+    return this.duration / 7;
+})
+
+// tourSchema.pre("save", function(next){
+//     this.slug = slugify(this.name, { lower: true });
+//     next();
+// })
+
+// tourSchema.pre("save", function(next){
+//     console.log("will save document...");
+//     next();
+// })
+
+// tourSchema.post("save", function(doc, next){
+//     console.log(doc);
+//     next();
+// })
+tourSchema.pre("find", function(next){
+    this.find({secretTour: {$ne: true}})
+    next();
+})
 
 const Tour = mongoose.model("Tour", tourSchema);
 
